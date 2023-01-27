c50d8c3c96ef8ab7bc1f64d4ec9437ef97f07e5d
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-01-27 13:00:52 +0200

    Add Tasks

---
+++ src/P05FunctionalProgramming/P01SortEvenNumbers.java
@@ -0,0 +1,41 @@
+package P05FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P01SortEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .map(elem -> Integer.parseInt(elem))
+                .collect(Collectors.toList());
+
+        numbers.removeIf(num -> {
+            // ...
+            // ...
+            return num % 2 != 0;
+        });
+
+        printWithSeparator(numbers);
+
+        System.out.println();
+        List<Integer> sortedNumbers = numbers
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+        printWithSeparator(sortedNumbers);
+    }
+
+    private static void printWithSeparator(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i));
+
+            if (i < numbers.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}

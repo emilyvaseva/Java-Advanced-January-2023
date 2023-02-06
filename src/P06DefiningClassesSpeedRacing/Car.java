29e6426276e0b9b5597a5cfdbb3edf369813589f
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-02-06 18:08:43 +0200

    Add more Exercises

---
+++ src/P06DefiningClassesSpeedRacing/Car.java
@@ -0,0 +1,30 @@
+package P06DefiningClassesSpeedRacing;
+
+public class Car {
+    private String model;
+    private double fuelAmount;
+    private double consumptionFuelPerKm;
+    private int distanceTraveled;
+
+    public Car(String model, double fuelAmount, double consumptionFuelPerKm) {
+        this.model = model;
+        this.fuelAmount = fuelAmount;
+        this.consumptionFuelPerKm = consumptionFuelPerKm;
+        this.distanceTraveled = 0;
+    }
+
+    public boolean drive(int kmDrive) {
+        double needFuel = kmDrive * this.consumptionFuelPerKm;
+        if (needFuel <= this.fuelAmount) {
+            this.fuelAmount -= needFuel;
+            this.distanceTraveled += kmDrive;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %d",this.model, this.fuelAmount, this.distanceTraveled);
+    }
+}
